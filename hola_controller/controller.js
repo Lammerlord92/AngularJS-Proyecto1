3645c592445a1858f9317a9b3dd55c7bba10424c
Lammerlord92 <javcarfer92@gmail.com>
2016-12-01 22:09:27 +0100

    Creados métodos básicos en el controlador, junto con datos en memoria

--- hola_controller/controller.js
+++ hola_controller/controller.js
@@ -2,4 +2,22 @@ var app=angular.module("MyFirstApp",[]);
 
 app.controller("FirstController",function($scope){
   $scope.nombre="Javier";
+  $scope.nuevoComentario={
+    comentario:"Nuevo comentario",
+    username:"Usuario"
+  }
+  $scope.comentarios=[
+    {
+      comentario:"Primer comentario",
+      username:"javcarfer"
+    },
+    {
+      comentario:"LOL",
+      username:"trollasso"
+    }
+  ];
+  $scope.agregarComentario=function(){
+    $scope.comentarios.push($scope.nuevoComentario);
+    $scope.nuevoComentario={};
+  }
 });
