f09a83ed294b287254239835fae5f1f36456923a
Lammerlord92 <javcarfer92@gmail.com>
2016-12-02 00:12:37 +0100

    Hecho post local

--- http/controller.js
+++ http/controller.js
@@ -2,6 +2,7 @@ var app=angular.module("MyFirstApp",[]);
 
 app.controller("FirstController",["$scope","$http",function(scope,http){
   scope.posts=[];
+  scope.newPost={};
   http.get("http://jsonplaceholder.typicode.com/posts")
   .then(function(data){
     console.log(data);
@@ -10,4 +11,18 @@ app.controller("FirstController",["$scope","$http",function(scope,http){
   function(data, status) {
      console.error('Response error', status, data);
   });
+  scope.addPost=function(){
+    http.post("http://jsonplaceholder.typicode.com/posts",{
+      title: scope.newPost.title,
+      body: scope.newPost.body,
+      userId: scope.newPost.userId
+    })
+    .then(function(data,status,headers,config){
+      scope.posts.unshift(scope.newPost);
+      scope.newPost={};
+    },
+    function(error,status,headers,config){
+      console.log(error);
+    });
+    }
 }]);
