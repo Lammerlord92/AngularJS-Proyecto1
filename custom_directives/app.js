2ffc845f370d1d235902efe4645132f18d95988d
Lammerlord92 <javcarfer92@gmail.com>
2016-12-15 13:49:57 +0100

    Creada directiva para buscar con autocompletado usando jquery

--- custom_directives/app.js
+++ custom_directives/app.js
@@ -1,6 +1,27 @@
 var app=angular.module("CustomDirective",[]);
-app.directive('circularImg',function(){
-  //scope:variables del controlador,element:elementos de la directiva(el div),attrs:los atributos
+app.directive("myAutocomplete",function(){
+  function postLink($scope,element,attrs){
+    $(element).autocomplete({
+      source:$scope.$eval(attrs.myAutocomplete),
+      select:function(ev,ui){
+        ev.preventDefault();
+        if(ui.item){
+          $scope.optionSelected(ui.item.value);
+        }
+      },
+      focus:function(ev,ui){
+        ev.preventDefault();
+        $(this).val(ui.item.label);
+      }
+    });
+  }
+  return {
+    //link:nombre de la función
+    link:postLink
+  }
+})
+.directive('circularImg',function(){
+  //scope:variables del controlador,element:elementos de la directiva(el div),attrs:los valores que le entran a la directiva
   return function(scope,element,attrs){
     //Value ews el valor que recibe la directiva
     attrs.$observe('circularImg',function(value){
@@ -12,11 +33,22 @@ app.directive('circularImg',function(){
     });
   }
 }).controller("AppCtrl",function($scope,$http){
+  $scope.repos=[];
   $http.get("https://api.github.com/users/Lammerlord92/repos")
   .then(function(data){
-    $scope.repos=data.data;
+    $scope.posts=data.data;
+    for(var i=data.data.length-1;i>=0;i--){
+      var repo=data.data[i];
+      $scope.repos.push(repo.name);
+    };
   },
   function(data, status) {
     console.error('Response error', status, data);
   });
+
+  $scope.optionSelected=function(data){
+      $scope.$apply(function(){
+        $scope.main_repo=data;
+      })
+  }
 });
