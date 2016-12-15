9a7a283ff49f0fe5f13ff42ff1bb6defecc9692d
Lammerlord92 <javcarfer92@gmail.com>
2016-12-15 12:21:20 +0100

    Creada directiva para ponerle estilo a una imagen

--- ng-src/app.js
+++ ng-src/app.js
@@ -1,9 +1,19 @@
 var app=angular.module("CustomDirective",[]);
-
-app.controller("AppCtrl",function($scope,$http){
+app.directive('circularImg',function(){
+  //scope:variables del controlador,element:elementos de la directiva(el div),attrs:los atributos
+  return function(scope,element,attrs){
+    //Value ews el valor que recibe la directiva
+    attrs.$observe('circularImg',function(value){
+      element.css({
+        "background":"url("+value+")",
+        "background-position":"center",
+        "background-size":"cover"
+      });
+    });
+  }
+}).controller("AppCtrl",function($scope,$http){
   $http.get("https://api.github.com/users/Lammerlord92/repos")
   .then(function(data){
-    console.log(data);
     $scope.repos=data.data;
   },
   function(data, status) {
